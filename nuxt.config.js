454f990366893a0bd4b7ae88053799b6acf36a2f
zhanghaofeng <892433342@qq.com>
2018-11-28 16:39:10 +0800

    增加图片懒加载

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    vendor: ['axios','element-ui'],
+    vendor: ['axios','element-ui','vue-lazyload'],
     extend (config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
